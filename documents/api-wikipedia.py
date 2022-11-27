e89f9b8f0f7360754eea9a83500670c3c8db738f
majo-erazo <75627494+majo-erazo@users.noreply.github.com>
2022-11-26 21:16:47 -0300

    .

--- documents/api-wikipedia.py
+++ documents/api-wikipedia.py
@@ -1,3 +1,28 @@
 import wikipedia
+import os
 
-print(wikipedia.search("coronavirus", results = 10, suggestion = True))
+os.makedirs('documents/doc1-5', exist_ok=True)
+os.makedirs('documents/doc6-10', exist_ok=True)
+
+# Se generan 10 titulos random
+result = wikipedia.random(10)
+#print(result)
+
+n = 1
+for i in result:
+    if n <= 5:
+        # Se verifica si el archivo ya existe para no sobreescribir
+        if os.path.isfile("documents/doc1-5/Archivo" + str(n) + ".txt") == False:
+            archivo = open("documents/doc1-5/Archivo" + str(n) + ".txt", "w")
+            # Se extrae el contenido de la pagina i
+            texto_page = wikipedia.page(i).content
+            # Se escribe en el archivo correspondiente
+            archivo.write(texto_page)
+            archivo.close
+    elif n > 5 and n <= 10:
+        if os.path.isfile("documents/doc1-5/Archivo" + str(n) + ".txt") == False:
+            archivo = open("documents/doc6-10/Archivo" + str(n) + ".txt", "w")
+            texto_page = wikipedia.page(i).content
+            archivo.write(texto_page)
+            archivo.close
+    n += 1 
