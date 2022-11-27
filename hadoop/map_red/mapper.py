4de118a77bb61a839ac2e65cde5140b638173ecc
nikkkaido <catalina.gomez@mail.udp.cl>
2022-11-27 16:18:30 -0300

    cambios en mapper.py y reducer.py

--- hadoop/map_red/mapper.py
+++ hadoop/map_red/mapper.py
@@ -3,13 +3,21 @@
 
 import sys
 import re
+import os
+
 
 for line in sys.stdin:
     line = re.sub(r'\W+',' ',line.strip())
     words = line.split()
 
+    ruta = os.environ['map_input_file']
+    lista_ruta = ruta.split("/", 6)
+    doc = lista_ruta[6].split('.', 1)[0]
+
     for word in words:
-        print('{}\t{}'.format(word,1))
+        print('imprime word, 1, doc en formato {}\\t{}\\t{}')
+        print('{}\t{}\t{}'.format(word,1,doc))
+
 
 
 
--- hadoop/map_red/reducer.py
+++ hadoop/map_red/reducer.py
@@ -4,29 +4,52 @@
 import sys
 
 current_word = None
+current_doc = None
 current_count = 0
 word = None
+<<<<<<< Updated upstream
 count_doc = 0
 
 mapa = {}
+=======
+doc = None
+>>>>>>> Stashed changes
 
 for line in sys.stdin:
     line = line.strip()
-    word, count = line.split('\t',1)
+    word, count, doc = line.split('\t',2)
 
+
+    dic_aux = {
+        doc:count
+    }
+
+    dic = {
+        word:dic_aux
+    }
+
+    
     try:
         count = int(count)
     except ValueError:
         continue
+    
+    
 
     if current_word == word:
         current_count += count
+        current_doc = doc
+        if current_word in dic:
+            if current_doc in dic[current_word]:
+                dic[current_word][current_doc] = current_count
+            else:
+                dic[current_doc] = current_count
     else:
         if current_word:
-            print('{}\t{}'.format(current_word,current_count))
+            print('{}\t{}\t{}'.format(current_word,current_count,current_doc))
         current_word = word
         current_count = count
-
+        current_doc = doc
 
     if word in mapa:
         if count_doc not in mapa[word]:
